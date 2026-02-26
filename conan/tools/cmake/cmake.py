3a37a8289b24a03cdd24566bd7da740ebd723058
Hivolve / Snarky Chipmunk <developer@hivolve.worknomic.ai>
2026-02-26 22:28:16 +0000

    feat: Add CMake.ctest() helper method and unit tests

--- conan/tools/cmake/cmake.py
+++ conan/tools/cmake/cmake.py
@@ -233,6 +233,49 @@ class CMake(object):
         self._build(build_type=build_type, target=target, cli_args=cli_args,
                     build_tool_args=build_tool_args, env=env)
 
+    def ctest(self, build_type=None, cli_args=None, env=""):
+        """
+        Equivalent to running ctest.
+
+        :param build_type: Use it only to override the value defined in the ``settings.build_type``.
+                           It can fail if the build is single configuration (e.g. Unix Makefiles), as
+                           in that case the build type must be specified at configure time, not build
+                           time.
+        :param cli_args: A list of arguments ``[arg1, arg2, ...]`` that will be passed to the
+                        ``ctest`` command directly.
+        :param env: Same as above ``build()``
+        """
+        if self._conanfile.conf.get("tools.build:skip_test", check_type=bool):
+            return
+
+        is_multi = is_multi_configuration(self._generator)
+        if build_type and not is_multi:
+            self._conanfile.output.error("Don't specify 'build_type' at build time for "
+                                         "single-config build systems")
+
+        bt = build_type or self._conanfile.settings.get_safe("build_type")
+        if not bt:
+            raise ConanException("build_type setting should be defined.")
+
+        ctest_program = self._conanfile.conf.get("tools.cmake:ctest_program", default="ctest")
+
+        arg_list = [ctest_program]
+        if is_multi and bt:
+            arg_list.extend(["--build-config", bt])
+
+        if cli_args:
+            arg_list.extend(cli_args)
+
+        command = " ".join(arg_list)
+
+        # Environment variables from [buildenv] and [runenv] are applied during execution.
+        # The default for ``test()`` is both the buildenv and the runenv
+        env = ["conanbuild", "conanrun"] if env == "" else env
+
+        self._conanfile.output.info("Running CMake.ctest()")
+        with chdir(self, self._conanfile.build_folder):
+            self._conanfile.run(command, env=env)
+
     @property
     def _compilation_verbosity_arg(self):
         """
---
+++ conans/test/unittests/tools/cmake/test_cmake_ctest.py
@@ -0,0 +1,67 @@
+import pytest
+from conan.tools.cmake import CMake
+from conan.tools.cmake.presets import write_cmake_presets
+from conans.client.conf import get_default_settings_yml
+from conans.model.conf import Conf
+from conans.model.settings import Settings
+from conans.test.utils.mocks import ConanFileMock
+from conans.test.utils.test_files import temp_folder
+
+
+def _setup_conanfile(generator, settings_dict=None):
+    settings = Settings.loads(get_default_settings_yml())
+    if settings_dict:
+        for k, v in settings_dict.items():
+            if k == "os":
+                settings.os = v
+            elif k == "build_type":
+                settings.build_type = v
+            elif k == "compiler":
+                settings.compiler = v
+    
+    conanfile = ConanFileMock()
+    conanfile.conf = Conf()
+    conanfile.folders.generators = "."
+    conanfile.folders.set_base_generators(temp_folder())
+    conanfile.settings = settings
+    
+    write_cmake_presets(conanfile, "toolchain", generator, {})
+    return conanfile
+
+
+def test_ctest_single_config():
+    conanfile = _setup_conanfile("Unix Makefiles", {"os": "Linux", "build_type": "Release"})
+    cmake = CMake(conanfile)
+    cmake.ctest()
+    assert "ctest" in conanfile.command
+    assert "--build-config" not in conanfile.command
+
+
+def test_ctest_multi_config():
+    conanfile = _setup_conanfile("Visual Studio 17 2022", {"os": "Windows", "build_type": "Debug"})
+    cmake = CMake(conanfile)
+    cmake.ctest()
+    assert "ctest --build-config Debug" in conanfile.command
+
+
+def test_ctest_custom_program():
+    conanfile = _setup_conanfile("Unix Makefiles", {"os": "Linux", "build_type": "Release"})
+    conanfile.conf.define("tools.cmake:ctest_program", "my_program")
+    cmake = CMake(conanfile)
+    cmake.ctest()
+    assert "my_program" in conanfile.command
+    assert "ctest" not in conanfile.command
+
+
+def test_ctest_cli_args():
+    conanfile = _setup_conanfile("Unix Makefiles", {"os": "Linux", "build_type": "Release"})
+    cmake = CMake(conanfile)
+    cmake.ctest(cli_args=["--verbose", "-R", "mytest"])
+    assert "ctest --verbose -R mytest" in conanfile.command
+
+
+def test_ctest_build_type_override():
+    conanfile = _setup_conanfile("Visual Studio 17 2022", {"os": "Windows", "build_type": "Debug"})
+    cmake = CMake(conanfile)
+    cmake.ctest(build_type="Release")
+    assert "ctest --build-config Release" in conanfile.command
