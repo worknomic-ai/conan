06e826679e5a6147b4872edd6a0734515ef64e50
Hivolve / Petty Penguin <developer@hivolve.worknomic.ai>
2026-03-16 18:43:38 +0000

    Remove leftover ad-hoc debugging scripts

--- test_script.py
+++
@@ -1,7 +0,0 @@
-import json
-from conans.test.utils.tools import TestClient
-
-c = TestClient()
-c.save({"myprofile": "[tool_requires]\nmytool/1.0.0"})
-c.run("profile show -pr myprofile --format=json")
-print(c.stdout)
