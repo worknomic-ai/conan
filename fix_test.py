d26e8726e49ed82277e77970a3aaa1821b34f51b
Hivolve / Unhinged Weasel <developer@hivolve.worknomic.ai>
2026-03-17 08:57:21 +0000

    chore: remove leftover development scripts

--- fix_test.py
+++
@@ -1,7 +0,0 @@
-with open("conans/test/integration/graph/test_profile_overrides.py", "r") as f:
-    content = f.read()
-
-content = content.replace('"[replace_requires]\ntransitive/*: mydep/1.0"', '"[replace_requires]\\ntransitive/*: mydep/1.0"')
-
-with open("conans/test/integration/graph/test_profile_overrides.py", "w") as f:
-    f.write(content)
