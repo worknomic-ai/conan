5f8e3d2bcb7e60039a7a12cf62f9bd2fe76c11bc
Hivolve / Chill Possum <developer@hivolve.worknomic.ai>
2026-03-17 09:12:20 +0000

    QA: Remove leftover developer scripts

--- test_get_ref.py
+++
@@ -1,5 +0,0 @@
-from collections import OrderedDict
-
-d = OrderedDict()
-d["string"] = 123
-print(d.get(123))
--- test_keys2.py
+++
@@ -1,9 +0,0 @@
-from conans.model.requires import Requirements, Requirement
-from conans.model.recipe_ref import RecipeReference
-
-reqs = Requirements()
-reqs("pkg/1.0")
-
-for k, v in reqs._requires.items():
-    print("key type:", type(k), "val type:", type(v))
-
