b1fbbfe0310cba5860e02f6bf774e1cd47eff36b
Hivolve / Unhinged Weasel <developer@hivolve.worknomic.ai>
2026-03-17 07:46:03 +0000

    developer: Functional Tests for Profile Overrides

---
+++ test3.py
@@ -0,0 +1,14 @@
+from conans.test.assets.genconanfile import GenConanfile
+from conans.test.utils.tools import TestClient
+
+client = TestClient()
+client.save({
+    "mydep/conanfile.py": GenConanfile("mydep", "1.0"),
+    "pkg/conanfile.py": GenConanfile("pkg", "1.0").with_require("dep/1.0"),
+    "profile": "[replace_requires]\ndep/*: mydep/1.0"
+})
+client.run("create mydep")
+client.run("create pkg -pr profile")
+for line in client.out.splitlines():
+    if "dep/1.0" in line:
+        print("MATCH:", line)
